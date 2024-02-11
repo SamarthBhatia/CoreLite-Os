d122bf9aee4fb14df8328b671982419bc9d5a024
S. <shreeshkumar2@gmail.com>
2024-02-12 00:00:46 +0530

    Minimal Kernal

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HEY: &[u8] = b"Hello Everyone I am BareMetal-OS Good to see Everyone";
+
 #[no_mangle]
 
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for(buff, &byte) in HEY.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(buff as isize * 2) = byte;
+            *vga_buffer.offset(buff as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
 
+ 
